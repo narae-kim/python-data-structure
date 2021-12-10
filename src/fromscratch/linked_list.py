061007735c9bd99c630c0efb598ca24c0517d690
Narae Kim <naraekimw@gmail.com>
2021-12-10 16:43:30 +0000

    Double linked list full implementation

--- src/fromscratch/linked_list.py
+++ src/fromscratch/linked_list.py
@@ -15,8 +15,7 @@ class LinkedList():
     def add(self, data):
         """
         Add new data at the end of the linked list.
-        :param data:
-        :return:
+        :param data: any data to insert at the end of the linked list
         """
         data_node = self.Node(data)
         if self.__first_node is None:
@@ -31,13 +30,15 @@ class LinkedList():
     def add_by_index(self, data, index):
         """
         Add new data at the index-th node in the linked list.
-        :param data:
-        :param index:
-        :return:
+        If the input is non-int type or negative value, ValueError will be raised.
+        :param data: any data to insert at the index-th node
+        :param index: non-negative int
         """
+        if type(index) is not int or index < 0:
+            raise ValueError("The index should be non-negative integer.")
         if index >= self.size:
             while index > self.size:
-                self.add(self.Node(None))
+                self.add(None)
             self.add(data)
         else:
             data_node = self.Node(data)
@@ -49,12 +50,70 @@ class LinkedList():
                         data_node.prev_node = nth_node.prev_node
                     nth_node.prev_node = data_node
                     data_node.next_node = nth_node
-                    nth_node = data_node
                     if i == 0:
-                        self.__first_node = nth_node
+                        self.__first_node = data_node
                     self.__size = self.size + 1
                     break
                 nth_node = nth_node.next_node
+
+    def get(self, index):
+        """
+        Get data at the index-th node in the linked list.
+        This method searches from the first node if the index is closer to the first node. Otherwise, it searches from the last node.
+        If the input is non-int type or negative value or above the size of the linked list, ValueError will be raised.
+        :param index: non-negative int which is smaller than the size of the linked list
+        :return: data at the index-th node
+        """
+        if type(index) is not int or index >= self.size or index < 0:
+            raise ValueError(
+                "The index should be non-negative integer and smaller than the size of the linked list ({}).".format(
+                    self.size))
+        if (self.size - 2 * index) >= 0:  # (size -1 - index) + (0 - index) > 0 then closer from the first node
+            nth_node = self.__first_node
+            for i in range(index + 1):
+                if i == index:
+                    return nth_node.data
+                nth_node = nth_node.next_node
+        else:
+            nth_node = self.__last_node
+            for i in range(self.size - 1, index - 1, -1):
+                if i == index:
+                    return nth_node.data
+                nth_node = nth_node.prev_node
+
+    def remove(self, index):
+        """
+        Remove data at the index-th node in the linked list.
+        This method searches from the first node if the index is closer to the first node. Otherwise, it searches from the last node.
+        If the input is non-int type or negative value, ValueError will be raised.
+        :param index: non-negative int
+        """
+        if type(index) is not int or index < 0:
+            raise ValueError("The index should be non-negative integer.")
+        if (self.size - 2 * index) >= 0:  # (size -1 - index) + (0 - index) > 0 then closer from the first node
+            nth_node = self.__first_node
+            for i in range(index + 1):
+                if i == index:
+                    nth_node.next_node.prev_node = nth_node.prev_node
+                    if i > 0:
+                        nth_node.prev_node.next_node = nth_node.next_node
+                    if index == 0:
+                        self.__first_node = nth_node.next_node
+                    self.__size = self.size - 1
+                    break
+                nth_node = nth_node.next_node
+        else:
+            nth_node = self.__last_node
+            for i in range(self.size - 1, index - 1, -1):
+                if i == index:
+                    nth_node.prev_node.next_node = nth_node.next_node
+                    if i < self.size - 1:
+                        nth_node.next_node.prev_node = nth_node.prev_node
+                    if index == self.size - 1:
+                        self.__last_node = nth_node.prev_node
+                    self.__size = self.size - 1
+                    break
+                nth_node = nth_node.prev_node
 
     def __str__(self):
         all_data_string = ""
@@ -96,7 +155,7 @@ class LinkedList():
 
         @prev_node.setter
         def prev_node(self, node):
-            if isinstance(node, LinkedList.Node):
+            if isinstance(node, LinkedList.Node) or node is None:
                 self.__prev_node = node
 
         @property
@@ -105,7 +164,7 @@ class LinkedList():
 
         @next_node.setter
         def next_node(self, node):
-            if isinstance(node, LinkedList.Node):
+            if isinstance(node, LinkedList.Node) or node is None:
                 self.__next_node = node
 
         def __str__(self):
@@ -123,19 +182,40 @@ if __name__ == '__main__':
     # print(node.prev_node)
     # print(node.next_node)
     # print(node2.prev_node)
+
     linked_list = LinkedList()
-    print(linked_list)
+    # print(linked_list)
     linked_list.add("First data")
-    print(linked_list)
-    print(reversed(linked_list))
+    # print(linked_list)
+    # print(reversed(linked_list))
     linked_list.add("Second data")
     linked_list.add("Third data")
-    print(linked_list)
-    print(reversed(linked_list))
+    # print(linked_list)
+    # print(reversed(linked_list))
     linked_list.add_by_index("New data", 2)
-    print(linked_list)
+    # print(linked_list)
     linked_list.add_by_index("New Start data", 0)
-    print(linked_list)
+    # print(linked_list)
     linked_list.add_by_index("New New data", 7)
     print(linked_list)
+    linked_list.add_by_index("Test data", 6)
     print(reversed(linked_list))
+    print(linked_list.get(0))
+    print(linked_list.get(3))
+    print(linked_list.get(8))
+    # print(linked_list.get(3.0))  # Error test
+    linked_list.remove(5)
+    print(linked_list)
+    linked_list.remove(7)
+    print(linked_list)
+    linked_list.remove(5)
+    print(linked_list)
+    linked_list.remove(2)
+    print(linked_list)
+    linked_list.remove(0)
+    print(linked_list)
+    linked_list.remove(1)
+    print(linked_list)
+    print(linked_list.get(0))
+    print(linked_list.get(1))
+    print(linked_list.get(2))
