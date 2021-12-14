d263351ca34af8cc20d2fd44ce7b273f7e4f15b8
Narae Kim <naraekimw@gmail.com>
2021-12-14 07:29:34 +0000

    Improve pop in Stack

--- src/fromscratch/stack.py
+++ src/fromscratch/stack.py
@@ -24,9 +24,15 @@ class Stack():
     def pop(self):
         """
         Pop the data at the top of the stack.
+        Return the data from the top of the stack if the stack is not empty. Otherwise, raise ValueError.
         """
-        self.__linked_list.remove(self.top_pointer)
-        self.__top_pointer = self.top_pointer - 1
+        if self.__top_pointer >= 0:
+            data = self.__linked_list.get(self.top_pointer)
+            self.__linked_list.remove()
+            self.__top_pointer = self.top_pointer - 1
+            return data
+        else:
+            raise ValueError("Stack is empty already.")
 
     def __str__(self):
         return str(self.__linked_list)
@@ -38,12 +44,12 @@ if __name__ == '__main__':
     stack.push(1)
     stack.push(2)
     print(stack)
-    stack.pop()
-    print(stack)
+    data = stack.pop()
+    print(stack, data)
     stack.push(3)
     print(stack)
-    stack.pop()
-    print(stack)
-    stack.pop()
-    print(stack)
+    data = stack.pop()
+    print(stack, data)
+    data = stack.pop()
+    print(stack, data)
     stack.pop()  # error test
